837f4f9bd5965df745ea843dfffe282e457b60a0
Sreelakshmi Pushpan <spushpa5@my.centennialcollege.ca>
2021-02-13 08:12:33 +0530

    initial commit

---
+++ public/Scripts/app.js
@@ -0,0 +1,11 @@
+//---------------------------Java script for count to option----------------------------------
+
+
+function redirecthome()
+{
+var form = document.getElementById("emailfrm");
+location.href = "/home";
+
+}
+
+  
---
+++ routes/index.js
@@ -0,0 +1,29 @@
+let express = require('express');
+let router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About' });
+});
+
+router.get('/services', function(req, res, next) {
+  res.render('services', { title: 'Services' });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact' });
+});
+
+router.get('/project', function(req, res, next) {
+  res.render('project', { title: 'Project' });
+});
+
+module.exports = router;
